b58f8795af02b6db3ddbf1ffaf217654a8560242
Ultra-pn4rv <tsarultra@gmail.com>
2024-05-29 08:40:06 -0400

    enemy controller modificado

---
+++ src/javascript/EnemyController.js
@@ -0,0 +1,146 @@
+import Enemy from './Enemy.js'
+import MovingDirection from './movingDirection.js'
+export default class EnemyController {
+enemyMap = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+[2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+[2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+];
+enemyRows = [];
+
+currentDirection = MovingDirection.right;
+xVelocity = 0;
+yVelocity = 0;
+defaultXVelocity = 1;
+defaultYVelocity = 1;
+moveDownTimerDefault = 30;
+moveDownTimer = this.moveDownTimerDefault;
+fireBulletTimerDefault = 100;
+fireBulletTimer = this.fireBulletTimerDefault;
+
+constructor(canvas, enemyBulletController, playerBulletController) {
+this.canvas = canvas;
+this.enemyBulletController = enemyBulletController;
+this.playerBulletController = playerBulletController;
+this.enemyDeathSound = new Audio("src/assets/sounds/enemy-death.wav");
+this.enemyDeathSound.volume = 0.1;
+
+this.createEnemies ();
+}
+
+draw(ctx) {
+    this.decrementMoveDownTimer();
+    this.updateVelocityAndDirection();
+    this.collisionDetection();
+    this.fireBullet();
+    this.drawEnemies(ctx);
+    this.resetMoveDownTimer();
+    this.fireBullet 
+}
+
+collisionDetection(){
+this.enemyRows.forEach((enemyRow) => {
+enemyRow.forEach((enemy, enemyIndex) =>{
+if (this.playerBulletController.collisionWith(enemy)) {
+this.enemyDeathSound.currentTime = 0;
+http://this.enemyDeathSound.play();
+enemyRow.splice(enemyIndex, 1);
+}
+});
+});
+this.enemyRows = this.enemyRows.filter((enemyRow) => enemyRow.lenght > 0);
+}
+fireBullet(){
+this.fireBulletTimer--;
+if(this.fireBulletTimer <= 0) {
+this.fireBulletTimer = this.fireBulletTimerDefault;
+const allEnemies = this.enemyRows.flat();
+const enemyIndex = Math.floor(Math.random() * allEnemies.lenght);
+const enemy = allEnemies[enemyIndex];
+this.enemyBulletController.shoot(enemy.x + enemy.width / 2, enemy.y, -3);
+}
+}
+
+resetMoveDownTimer() {
+if(this.moveDownTimer <= 0 ) {
+this.moveDownTimer = this.moveDownTimerDefault;
+}
+}
+
+decrementMoveDownTimer(){
+if(
+this.currentDirection === MovingDirection.downLeft ||
+this.currentDirection === MovingDirection.downRight
+){
+this.moveDownTimer--;
+}
+}
+
+updateVelocityAndDirection() {
+for(const enemyRow of this.enemyRows) {
+if(this.currentDirection === MovingDirection.right) {
+this.xVelocity = this.defaultXVelocity;
+this.yVelocity = 0;
+const rightMostEnemy = enemyRow[enemyRow.length - 1];
+if(rightMostEnemy.x + rightMostEnemy.width >= this.canvas.width){
+this.currentDirection = MovingDirection.downLeft;
+break;
+}
+} else if(this.currentDirection === MovingDirection.downLeft) {
+if(this.moveDown(MoveDirection.left)) {
+break;
+}
+} else if(this.currentDirection === MovingDirection.left) {
+this.xVelocity = -this.defaultXVelocity;
+this.yVelocity = 0
+const leftMostEnemy = enemyRow[0];
+if(leftMostEnemy.x <= 0) {
+this.currentDirection = MovingDirection.downRight;
+break;
+}
+} else if (this.currentDirection === MovingDirection.downRight) {
+if(this.moveDown(Moving)){
+break;
+}
+}
+}
+}
+
+moveDown(newDirection) {
+this.xVelocity = 0;
+this.yVelocity = this.defaultYVelocity;
+if(this.moveDownTimer <= 0) {
+this.currentDirection = newDirection;
+return true;
+} else  {
+return false;
+}
+}
+
+drawEnemies(ctx) {
+this.enemyRows.flat().forEach((enemyRow) => {
+enemyRow.move(this.xVelocity, this.yVelocity)
+enemyRow.draw(ctx);
+});
+}
+
+happy = () => {};
+ 
+createEnemies(ctx) {
+    this.enemyMap.forEach((row, rowIndex) => {
+        this.enemyRows[rowIndex] = [];
+        row.forEach((enemyNumber, enemyIndex) => {
+            if(enemyNumber > 0) {
+                this.enemyRows[rowIndex].push = (new Enemy(enemyIndex * 35, enemyNumber));
+            }
+        })
+    })
+} 
+
+
+ collideWith(sprite){
+    return this.enemyRows.flat().some((enemy) => enemy.collideWith(sprite));
+ }
+}
