57781ee7675a0d970eedb6742cd62691b82e01b5
Ultra-pn4rv <tsarultra@gmail.com>
2024-06-05 08:35:20 -0400

    qualquer coisa

--- src/javascript/Enemy.js
+++ src/javascript/Enemy.js
@@ -1,30 +1,32 @@
-class Enemy {
-    constructor(canvas, x, y, imageNumber) {
-        this.x = x;
-        this.y = y;
-        this.width = 44;
-        this.height = 32;
-        this.image = new Image();
-        this.image.src = `src/assets/images/enemy${imageNumber}.png`;
-}
-
-draw(ctx) {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-}
-move(xVelocity, yVelocity) {
-    this.x += xVelocity;
-    this.y += yVelocity;
-}
-collideWith(sprite) {
-    if(
+export default class Enemy {
+    constructor(x, y, imageNumber) {
+      this.x = x;
+      this.y = y;
+      this.width = 44;
+      this.height = 32;
+      this.image = new Image();
+      this.image.src = `src/assets/images/enemy${imageNumber}.png`;
+    }
+  
+    draw(ctx) {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+  
+    move(xVelocity, yVelocity) {
+      this.x += xVelocity;
+      this.y += yVelocity;
+    }
+  
+    collideWith(sprite) {
+      if(
         this.x + this.width > sprite.x &&
         this.x < sprite.x + sprite.width &&
         this.y + this.height > sprite.y &&
         this.y < sprite.y + sprite.height
-    ) {
+      ) {
         return true;
-    } else {
+      } else {
         return false;
+      }
     }
-    }
-}
+  }
--- src/javascript/bulletControler.js
+++ src/javascript/bulletControler.js
@@ -1,44 +1,57 @@
 import Bullet from "./Bullet.js";
-export default class BulletControler {
+
+export default class BulletController {
     bullets = [];
-    timeTillNextBulletAllowed = 0
-    constructor(canvas, maxBulletAtTime, bulletColor, soundenabled) {
+    timeTillNextBulletAllowed = 0;
+
+    constructor(canvas, maxBulletsAtATime, bulletColor, soundEnabled) {
         this.canvas = canvas;
-        this.maxBulletAtTime = maxBulletAtTime;
+        this.maxBulletsAtATime = maxBulletsAtATime;
         this.bulletColor = bulletColor;
-        this.soundenabled = soundenabled;
+        this.soundEnabled = soundEnabled;
 
         this.shootSound = new Audio("src/assets/sounds/shoot.wav");
         this.shootSound.volume = 0.1;
     }
 
     draw(ctx) {
-        this.bullets = this.bullets.filter((bullet) => bullet.y + bullet.width > 0 && bullet.y <= this.canvas.height);
-    
-       this.bullet.forEach((bullet) => bullet.draw(ctx));
-       if(this.timeFillNewBulletAllowod > 0 ) {
-        this.timeFillNewBulletAllowod--; 
-       }
-    }
-    collideWith(sprite) {
-        const bulletThatWithSpriteIndex = this.bullets.findIndex((bullet) => bullet.collideWith(sprite));
+        this.bullets = this.bullets.filter(
+            (bullet) => bullet.y + bullet.width > 0 && bullet.y <= this.canvas.height
+        );
 
-        if(bulletThatWithSpriteIndex >= 0) {
-            this.bullets.splice(bulletThatWithSpriteIndex, 1);
+        this.bullets.forEach((bullet) => bullet.draw(ctx));
+
+        if (this.timeTillNextBulletAllowed > 0) {
+            this.timeTillNextBulletAllowed--;
+        }
+    }
+
+    collideWith(sprite) {
+        const bulletThatHitSpriteIndex = this.bullets.findIndex((bullet) => 
+            bullet.collideWith(sprite)
+        );
+
+        if (bulletThatHitSpriteIndex >= 0) {
+            this.bullets.splice(bulletThatHitSpriteIndex, 1);
             return true;
         }
+
         return false;
     }
-    shoot(x, y, velocity, timeFillNextBulletAllowed = 0) {
-        if(this.bullets.length < this.maxBulletAtTime) {
+
+    shoot(x, y, velocity, timeTillNextBulletAllowed = 0) {
+        if (this.timeTillNextBulletAllowed <= 0 && 
+            this.bullets.length < this.maxBulletsAtATime) {
+            
             const bullet = new Bullet(this.canvas, x, y, velocity, this.bulletColor);
             this.bullets.push(bullet);
-            if(this.soundEnabled){
+
+            if (this.soundEnabled) {
                 this.shootSound.currentTime = 0;
-                this.shootSound.play = 0;
                 this.shootSound.play();
             }
-            this.timeFillNewBulletAllowed = timeFillNextBulletAllowed;
-        } 
+
+            this.timeTillNextBulletAllowed = timeTillNextBulletAllowed;
+        }
     }
 }
