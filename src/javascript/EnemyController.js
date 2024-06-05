38875f4343b8b6418754f789effa579368a6e54c
Ultra-pn4rv <tsarultra@gmail.com>
2024-06-05 08:49:35 -0400

    finish game

--- src/javascript/EnemyController.js
+++ src/javascript/EnemyController.js
@@ -1,148 +1,146 @@
 import Enemy from './Enemy.js';
-import MovingDirection from './movingDirection.js';
+import MovingDirection from './MovingDirection.js';
 
 export default class EnemyController {
-    enemyMap = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 2, 2, 2, 2, 2, 2, 2, 2],
-        [2, 2, 2, 2, 2, 2, 2, 2, 2],
-        [2, 2, 2, 2, 2, 2, 2, 2, 2],
-    ];
-    enemyRows = [];
+  enemyMap = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+    [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  ];
+  enemyRows = [];
 
-    currentDirection = MovingDirection.right;
-    xVelocity = 0;
-    yVelocity = 0;
-    defaultXVelocity = 1;
-    defaultYVelocity = 1;
-    moveDownTimerDefault = 30;
-    moveDownTimer = this.moveDownTimerDefault;
-    fireBulletTimerDefault = 100;
-    fireBulletTimer = this.fireBulletTimerDefault;
+  currentDirection = MovingDirection.right;
+  xVelocity = 0;
+  yVelocity = 0;
+  defaultXVelocity = 1;
+  defaultYVelocity = 1;
+  moveDownTimerDefault = 30;
+  moveDownTimer = this.moveDownTimerDefault;
+  fireBulletTimerDefault = 100;
+  fireBulletTimer = this.fireBulletTimerDefault;
 
-    constructor(canvas, enemyBulletController, playerBulletController) {
-        this.canvas = canvas;
-        this.enemyBulletController = enemyBulletController;
-        this.playerBulletController = playerBulletController;
-        this.enemyDeathSound = new Audio("src/assets/sounds/enemy-death.wav");
-        this.enemyDeathSound.volume = 0.1;
+  constructor(canvas, enemyBulletController, playerBulletController) {
+    this.canvas = canvas;
+    this.enemyBulletController = enemyBulletController;
+    this.playerBulletController = playerBulletController;
+    this.enemyDeathSound = new Audio("src/assets/sounds/enemy-death.wav");
+    this.enemyDeathSound.volume = 0.1;
 
-        this.createEnemies();
-    }
+    this.createEnemies();
+  }
 
-    draw(ctx) {
-        this.decrementMoveDownTimer();
-        this.updateVelocityAndDirection();
-        this.collisionDetection();
-        this.drawEnemies(ctx);
-        this.resetMoveDownTimer();
-        this.fireBullet();
-    }
+  draw(ctx) {
+    this.decrementMoveDownTimer();
+    this.updateVelocityAndDirection();
+    this.collissionDetection();
+    this.drawEnemies(ctx);
+    this.resetMoveDownTimer();
+    this.fireBullet();
+  }
 
-    collisionDetection() {
-        this.enemyRows.forEach((enemyRow) => {
-            enemyRow.forEach((enemy, enemyIndex) => {
-                if (this.playerBulletController.collideWith(enemy)) {
-                    this.enemyDeathSound.currentTime = 0;
-                    http://this.enemyDeathSound.play();
-                    enemyRow.splice(enemyIndex, 1);
-                }
-            });
-        });
-        this.enemyRows = this.enemyRows.filter((enemyRow) => enemyRow.length > 0);
-    }
-
-    fireBullet() {
-        this.fireBulletTimer--;
-        if (this.fireBulletTimer <= 0) {
-            this.fireBulletTimer = this.fireBulletTimerDefault;
-            const allEnemies = this.enemyRows.flat();
-            const enemyIndex = Math.floor(Math.random() * allEnemies.length);
-            const enemy = allEnemies[enemyIndex];
-            this.enemyBulletController.shoot(enemy.x + enemy.width / 2, enemy.y, -3);
+  collissionDetection() {
+    this.enemyRows.forEach((enemyRow) => {
+      enemyRow.forEach((enemy, enemyIndex) => {
+        if(this.playerBulletController.collideWith(enemy)) {
+          this.enemyDeathSound.currentTime = 0;
+          this.enemyDeathSound.play();
+          enemyRow.splice(enemyIndex, 1);
         }
-    }
+      });
+    });
+    this.enemyRows = this.enemyRows.filter((enemyRow) => enemyRow.length > 0);
+  }
 
-    resetMoveDownTimer() {
-        if (this.moveDownTimer <= 0) {
-            this.moveDownTimer = this.moveDownTimerDefault;
+  fireBullet(){
+    this.fireBulletTimer--;
+    if(this.fireBulletTimer <= 0) {
+      this.fireBulletTimer = this.fireBulletTimerDefault;
+      const allEnemies = this.enemyRows.flat();
+      const enemyIndex = Math.floor(Math.random() * allEnemies.length);
+      const enemy =  allEnemies[enemyIndex];
+      this.enemyBulletController.shoot(enemy.x + enemy.width / 2, enemy.y, -3);
+    }
+  }
+
+  resetMoveDownTimer() {
+    if(this.moveDownTimer <= 0 ) {
+      this.moveDownTimer = this.moveDownTimerDefault;
+    }
+  }
+
+  decrementMoveDownTimer() {
+    if(
+      this.currentDirection === MovingDirection.downLeft ||
+      this.currentDirection === MovingDirection.downRight
+    ) {
+      this.moveDownTimer--;
+    }
+  }
+
+  updateVelocityAndDirection() {
+    for(const enemyRow of this.enemyRows) {
+      if(this.currentDirection === MovingDirection.right) {
+        this.xVelocity = this.defaultXVelocity;
+        this.yVelocity = 0;
+        const rightMostEnemy =  enemyRow[enemyRow.length - 1];
+        if(rightMostEnemy.x + rightMostEnemy.width >= this.canvas.width) {
+          this.currentDirection = MovingDirection.downLeft;
+          break;
         }
-    }
-
-    decrementMoveDownTimer() {
-        if (
-            this.currentDirection === MovingDirection.downLeft ||
-            this.currentDirection === MovingDirection.downRight
-        ) {
-            this.moveDownTimer--;
+      } else if(this.currentDirection === MovingDirection.downLeft) {
+        if(this.moveDown(MovingDirection.left)) {
+          break;
         }
-    }
-
-    updateVelocityAndDirection() {
-        for (const enemyRow of this.enemyRows) {
-            if (this.currentDirection === MovingDirection.right) {
-                this.xVelocity = this.defaultXVelocity;
-                this.yVelocity = 0;
-                const rightMostEnemy = enemyRow[enemyRow.length - 1];
-                if (rightMostEnemy.x + rightMostEnemy.width > this.canvas.width) {
-                    this.currentDirection = MovingDirection.downLeft;
-                    break;
-                }
-            } else if (this.currentDirection === MovingDirection.downLeft) {
-                if (this.moveDown(MovingDirection.left)) {
-                    break;
-                }
-            } else if (this.currentDirection === MovingDirection.left) {
-                this.xVelocity = -this.defaultXVelocity;
-                this.yVelocity = 0;
-                const leftMostEnemy = enemyRow[0];
-                if (leftMostEnemy.x < 0) {
-                    this.currentDirection = MovingDirection.downRight;
-                    break;
-                }
-            } else if (this.currentDirection === MovingDirection.downRight) {
-                if (this.moveDown(MovingDirection.right)) {
-                    break;
-                }
-            }
+      } else if(this.currentDirection === MovingDirection.left) {
+        this.xVelocity = -this.defaultXVelocity;
+        this.yVelocity = 0;
+        const leftMostEnemy = enemyRow[0];
+        if (leftMostEnemy.x <= 0) {
+          this.currentDirection = MovingDirection.downRight;
+          break;
         }
-    }
-
-    moveDown(newDirection) {
-        this.xVelocity = 0;
-        this.yVelocity = this.defaultYVelocity;
-        if (this.moveDownTimer <= 0) {
-            this.currentDirection = newDirection;
-            return true;
-        } else {
-            return false;
+      } else if( this.currentDirection === MovingDirection.downRight) {
+        if(this.moveDown(MovingDirection.right)) {
+          break;
         }
+      }
     }
+  }
 
-    drawEnemies(ctx) {
-        this.enemyRows.flat().forEach((enemy) => {
-            enemy.move(this.xVelocity, this.yVelocity);
-            enemy.draw(ctx);
-        });
+  moveDown(newDirection) {
+    this.xVelocity = 0;
+    this.yVelocity = this.defaultYVelocity;
+    if(this.moveDownTimer <= 0) {
+      this.currentDirection = newDirection;
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    happy = () => {};
+  drawEnemies(ctx) {
+    this.enemyRows.flat().forEach((enemy) => {
+      enemy.move(this.xVelocity, this.yVelocity);
+      enemy.draw(ctx);
+    });
+  }
 
-    createEnemies() {
-        this.enemyMap.forEach((row, rowIndex) => {
-            this.enemyRows[rowIndex] = [];
-            row.forEach((enemyNumber, enemyIndex) => {
-                if (enemyNumber > 0) {
-                    this.enemyRows[rowIndex].push(new Enemy(enemyIndex * 50, rowIndex * 35, enemyNumber));
-                }
-            });
-        });
-    }
+  happy = () => {};
 
-    collideWith(sprite) {
-        return this.enemyRows.flat().some((enemy) => enemy.collideWith(sprite));
-    }
+  createEnemies() {
+    this.enemyMap.forEach((row, rowIndex) => {
+      this.enemyRows[rowIndex] = [];
+      row.forEach((enemyNumber, enemyIndex) => {
+        if(enemyNumber > 0) {
+          this.enemyRows[rowIndex].push(new Enemy(enemyIndex * 50, rowIndex * 35, enemyNumber));
+        }
+      });
+    }); 
+  }
+  collideWith(sprite) {
+    return this.enemyRows.flat().some((enemy) => enemy.collideWith(sprite));
+  }
 }
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,59 +1,70 @@
-import EnemyController from './EnemyController.js';
-import BulletController from './BulletController.js';
-import Player from './player.js';
+import EnemyController from "./EnemyController.js";
+import BulletController from "./BulletController.js";
+import Player from "./Player.js";
 
-const canvas = document.getElementById('game');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
 canvas.width = 600;
 canvas.height = 600;
 
 const background = new Image();
-background.src = 'src/assets/images/space.png';
+background.src = "./src/assets/images/space.png";
 
 const enemyBulletController = new BulletController(canvas, 4, "red", false);
 const playerBulletController = new BulletController(canvas, 10, "white", true);
 
-const enemyController = new EnemyController(canvas, enemyBulletController, playerBulletController);
-const player = new Player(canvas, 10, playerBulletController); 
+const enemyController = new EnemyController(
+  canvas,
+  enemyBulletController,
+  playerBulletController
+);
+
+const player = new Player(canvas, 10, playerBulletController);
 
 let isGameOver = false;
 let didWin = false;
 
 function game() {
-    checkGameOver();
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    
-    if(!isGameOver) {
-        enemyController.draw(ctx);
-        player.draw(ctx);
-        playerBulletController.draw(ctx);
-        enemyBulletController.draw(ctx);
-    } else {
-        displayGameOver();
-    }
+  checkGameOver();
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  displayGameOver();
+
+  if(!isGameOver) {
+    enemyController.draw(ctx);
+    player.draw(ctx);
+    playerBulletController.draw(ctx);
+    enemyBulletController.draw(ctx);
+  }
 }
 
 function displayGameOver() {
-    let text = didWin ? "Você Ganhou!" : "Game Over";
+  if(isGameOver){
+    let  text = didWin ? "Você Ganhou!" : "Game Over";
     let textOffset = didWin ? 5 : 3.6;
-    
     ctx.fillStyle = "white";
     ctx.font = "35px 'Press Start 2P'";
-    ctx.fillText(text, canvas.width / textOffset, canvas.height / 2);   
+    ctx.fillText(text, canvas.width / textOffset, canvas.height / 2);
+  }
 }
 
 function checkGameOver() {
-    if(isGameOver) {
-        return;
-    }
-    if(enemyBulletController.collideWith(player) || enemyController.collideWith(player)) {
-        isGameOver = true;
-    }
-    if(enemyController.enemyRows.length === 0) {
-        didWin = true;
-        isGameOver = true;
-    }
+  if(isGameOver)  {
+    return;
+  }
+
+  if(enemyBulletController.collideWith(player)) {
+    isGameOver = true;
+  }
+
+  if(enemyController.collideWith(player)) {
+    isGameOver = true;
+  }
+
+  if(enemyController.enemyRows.length === 0) {
+    didWin = true;
+    isGameOver = true;
+  }
 }
 
 setInterval(game, 1000 / 60);
