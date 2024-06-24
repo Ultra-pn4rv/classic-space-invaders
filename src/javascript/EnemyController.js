cd74f128548ab1e0d0c11fc5ae8afc27fc8fe58e
Ultra-pn4rv <tsarultra@gmail.com>
2024-06-24 08:17:31 -0400

    .

--- src/javascript/EnemyController.js
+++ src/javascript/EnemyController.js
@@ -16,11 +16,11 @@ export default class EnemyController {
     currentDirection = MovingDirection.right;
     xVelocity = 0;
     yVelocity = 0;
-    defaultXVelocity = 15;
-    defaultYVelocity = 15;
-    moveDownTimerDefault = 3;
+    defaultXVelocity = 7;
+    defaultYVelocity = 3.5;
+    moveDownTimerDefault = 4;
     moveDownTimer = this.moveDownTimerDefault;
-    fireBulletTimerDefault = 1;
+    fireBulletTimerDefault = 0.1;
     fireBulletTimer = this.fireBulletTimerDefault;
 
     constructor(canvas, enemyBulletController, playerBulletController) {
